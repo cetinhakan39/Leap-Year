26f799d3f85df21525ac18dade53b35d3d571cfa
cetinhakan39 <cetinhakan39@gmail.com>
2024-05-15 22:21:33 +0300

    Leap Year Calculator

--- LeapYear/ViewController.swift
+++ LeapYear/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var textLabel: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func button(_ sender: UIButton) {
+        guard let yearText = textLabel.text,
+                      let year = Int(yearText) else {
+                    label.text = "Geçersiz yıl"
+                    return
+                }
+                // Artık yıl kontrolü yapılıyor
+                if isLeap(year: year) {
+                    label.text = "Artık yıl"
+                } else {
+                    label.text = "Artık yıl değil"
+                }
     }
-
-
+    
+    func isLeap(year: Int) -> Bool {
+            return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+        }
+    
 }
 
